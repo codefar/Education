917b8f7956188c7ebdbab3bcfcf59f3e3479dedb
wangyh <1051522865@qq.com>
2015-06-06 00:20:06 +0800

    添加主结构Fragment

--- code/src/com/education/MainActivity.java
+++ code/src/com/education/MainActivity.java
@@ -1,20 +1,23 @@
 package com.education;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
+
+import com.education.ManualTimFragment;
+import com.education.PersonCenterFragment;
+import com.education.R;
+import com.education.SmartRecomentFragment;
+import com.education.VolunteerCollectionFragment;
 
 public class MainActivity extends FragmentActivity {
 
 	private FragmentTabHost mTabHost;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,57 +25,30 @@ public class MainActivity extends FragmentActivity {
 		initTabs();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private void initTabs() {
+		mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+		mTabHost.setup(this, getSupportFragmentManager(),
+				R.id.realtabcontent);
+
+		View tab_smart = getLayoutInflater().inflate(
+				R.layout.view_main_tab_smart, null);
+		View tab_manual = getLayoutInflater().inflate(
+				R.layout.view_main_tab_manual, null);
+		View tab_volunteer = getLayoutInflater().inflate(
+				R.layout.view_main_tab_volunteer, null);
+		View tab_center = getLayoutInflater().inflate(
+				R.layout.view_main_tab_center, null);
+
+		mTabHost.addTab(mTabHost.newTabSpec("tab_smart").setIndicator(tab_smart),
+				SmartRecomentFragment.class, null);
+		mTabHost.addTab(mTabHost.newTabSpec("tab_manual").setIndicator(tab_manual),
+				ManualTimFragment.class, null);
+		mTabHost.addTab(mTabHost.newTabSpec("tab_volunteer").setIndicator(tab_volunteer),
+				VolunteerCollectionFragment.class, null);
+		mTabHost.addTab(mTabHost.newTabSpec("tab_center").setIndicator(tab_center),
+				PersonCenterFragment.class, null);
+		// 设置tabs之间的分隔线不显示
+		mTabHost.getTabWidget().setShowDividers(LinearLayout.SHOW_DIVIDER_NONE);
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
-	
-	private void initTabs(){  
-        mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);  
-        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);    
-  
-        View tab_smart = getLayoutInflater().inflate(R.layout.view_main_tab_smart, null);
-        View tab_manual = getLayoutInflater().inflate(R.layout.view_main_tab_manual, null);
-        View tab_volunteer = getLayoutInflater().inflate(R.layout.view_main_tab_volunteer, null);  
-        View tab_center = getLayoutInflater().inflate(R.layout.view_main_tab_center, null);  
-  
-        mTabHost.addTab(mTabHost.newTabSpec("").setIndicator(tab_smart), PlaceholderFragment.class, null);
-        mTabHost.addTab(mTabHost.newTabSpec("").setIndicator(tab_manual), PlaceholderFragment.class, null);
-        mTabHost.addTab(mTabHost.newTabSpec("").setIndicator(tab_volunteer), PlaceholderFragment.class, null);  
-        mTabHost.addTab(mTabHost.newTabSpec("").setIndicator(tab_center), PlaceholderFragment.class, null);  
-        //设置tabs之间的分隔线不显示  
-        mTabHost.getTabWidget().setShowDividers(LinearLayout.SHOW_DIVIDER_NONE);
-    }  
-	
 }
---
+++ code/src/com/education/PersonCenterFragment.java
@@ -0,0 +1,29 @@
+package com.education;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PersonCenterFragment extends Fragment {
+
+    /**
+     * When creating, retrieve this instance's number from its arguments.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    /**
+     * The Fragment's UI is just a simple text view showing its
+     * instance number.
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main_center, container, false);
+        return v;
+    }
+}
