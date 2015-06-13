16aae87c3269ad8b16c45da6646c64820621ba0a
苏兆强 <suzhaoqiang@souyidai.com>
2015-06-13 15:00:22 +0800

    检查答题情况的接口调通

--- code/src/com/education/TestActivity.java
+++ code/src/com/education/TestActivity.java
@@ -91,8 +91,45 @@ public class TestActivity extends CommonBaseActivity {
 //        shaiXuanByMajor();
 //        tuiJianXinXi();
 
-        zhuanYeFenXiBaoGao();
+//        zhuanYeFenXiBaoGao();
+        huiDaZhuangTai();
     }
+
+    //回答状态
+    private void huiDaZhuangTai() {
+        final FastJsonRequest request = new FastJsonRequest(Request.Method.POST, Url.XING_GE_CE_SHI_HUI_DA_ZHUANG_TAI
+                , null, new VolleyResponseListener(this) {
+            @Override
+            public void onSuccessfulResponse(JSONObject response, boolean success) {
+                if (success) {
+                    int status = response.getInteger("status");
+                    if (status == 1) {
+                        Toast.makeText(TestActivity.this, "回答完毕", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(TestActivity.this, "未曾回答", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    ErrorData errorData = AppHelper.getErrorData(response);
+                    Toast.makeText(TestActivity.this, errorData.getText(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new VolleyErrorListener() {
+            @Override
+            public void onVolleyErrorResponse(VolleyError volleyError) {
+                LogUtil.logNetworkResponse(volleyError, TAG);
+                Toast.makeText(TestActivity.this, getResources().getString(R.string.internet_exception), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("userId", User.getInstance().getId());
+                return AppHelper.makeSimpleData("start", map);
+            }
+        };
+        EduApp.sRequestQueue.add(request);
+    }
+
 
     private void setData() {
         int year = 4;
